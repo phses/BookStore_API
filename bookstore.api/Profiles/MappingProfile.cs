d5756dc698119f66d0bc3e4d13bf2fe87b5b2646
Phses <phsouzaesilva@gmail.com>
2022-12-14 11:58:33 -0300

    feat: add Mapping resquestToEntitie e responseToEntitie

---
+++ bookstore.api/Profiles/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using bookstore.Domain.Contracts.Request;
+using bookstore.Domain.Contracts.Response;
+using bookstore.Domain.Entities;
+
+namespace bookstore.api.Profiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            #region EntityToRequest
+            CreateMap<AvaliacaoRequest, Avaliacao>().ReverseMap();
+            CreateMap<EnderecoRequest, Endereco>().ReverseMap();
+            CreateMap<FornecedorRequest, Fornecedor>().ReverseMap();
+            CreateMap<LivroRequest, Livro>().ReverseMap();
+            CreateMap<PedidoRequest, Pedido>().ReverseMap();
+            CreateMap<UsuarioRequest, Usuario>().ReverseMap();
+            #endregion
+
+            #region EntityToResponse
+            CreateMap<AvaliacaoResponse, Avaliacao>().ReverseMap();
+            CreateMap<EnderecoResponse, Endereco>().ReverseMap();
+            CreateMap<FornecedorResponse, Fornecedor>().ReverseMap();
+            CreateMap<LivroResponse, Livro>().ReverseMap();
+            CreateMap<PedidoResponse, Pedido>().ReverseMap();
+            CreateMap<UsuarioResponse, Usuario>().ReverseMap();
+            #endregion
+        }
+
+    }
+}
