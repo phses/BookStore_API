f57ca472c246aef2db24e663576d38ff23a4913d
Phses <phsouzaesilva@gmail.com>
2022-12-15 14:10:10 -0300

    feat: add BookStoreContext em infrastructure

---
+++ bookstore.Infrastructure/Contexts/BookStoreContext.cs
@@ -0,0 +1,28 @@
+﻿using bookstore.Domain.Entities;
+using bookstore.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace bookstore.Infrastructure.Contexts
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<LivroPedido> LivroPedidos { get; set; }
+        public DbSet<LivroAvaliacao> LivroAvaliacoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LivroPedido>(new LivroPedidoMap().Configure);
+            modelBuilder.Entity<LivroAvaliacao>(new LivroAvaliacaoMap().Configure);
+        }
+        
+    }
+}
