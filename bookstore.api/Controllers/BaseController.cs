3da29c8a9523cbf2dbdd9602002a8ba3143916b4
Phses <phsouzaesilva@gmail.com>
2022-12-23 07:08:35 -0300

    feat: add conventiontype controller

--- bookstore.api/Controllers/BaseController.cs
+++ bookstore.api/Controllers/BaseController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace bookstore.api.Controllers
 {
     [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     [Route("[controller]")]
     public class BaseController<TEntity, KRequest, YResponse> : ControllerBase where TEntity : Entity
     {
